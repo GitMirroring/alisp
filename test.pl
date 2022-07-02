41d40e1fc4e2f0a8d5a641290d066a22d2178ad4
Andrea G. Monaco <andrea.monaco@autistici.org>
2022-07-02 14:40:22 +0200

    Add test script.  First perl code I've ever written, very cool

---
+++ test.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl -w
+
+# Copyright (C) 2022 Andrea G. Monaco
+# 
+# This file is part of al.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+use IPC::Open2;
+
+
+
+my $total_tests = 0;
+
+my $passed_tests = 0;
+
+my $failed_tests = 0;
+
+
+my $pid = open2 (my $al_out, my $al_in, './al');
+
+
+
+for ((1..5))
+{
+    <$al_out>;
+}
+
+
+make_test ("nil", "()");
+make_test ("NIL", "()");
+make_test ("t", "T");
+make_test ("\"\"", "\"\"");
+
+
+
+print "\n\ntotal tests: " . $total_tests . "\n";
+print "passed " . $passed_tests . " (" . $passed_tests / $total_tests * 100 . "%), failed " . $failed_tests . "\n";
+
+
+
+kill 'TERM', $pid;
+waitpid ($pid, 0);
+
+
+
+
+sub make_test
+{
+    print $_[0] . " -> ";
+    print $al_in $_[0] . "\n";
+
+    <$al_out>;
+
+    $out = <$al_out>;
+
+    chomp ($out);
+
+    print $out;
+    
+    if ($out eq $_[1])
+    {
+	print "; OK\n";
+	$passed_tests++;
+    }
+    else
+    {
+	print "; FAIL, expected " . $_[1] . " instead\n";
+	$failed_tests++;
+    }
+
+    $total_tests++;
+}
