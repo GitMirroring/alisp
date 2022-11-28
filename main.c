22a220fc42f9ed1c1a8b6a88eba41ef91b6a1acf
Andrea G. Monaco <andrea.monaco@autistici.org>
2022-11-28 16:04:42 +0100

    Break long line

--- main.c
+++ main.c
@@ -9118,7 +9118,8 @@ print_version (void)
 {
   puts ("al " PACKAGE_VERSION "\n"
 	"Copyright (C) 2022 Andrea G. Monaco\n"
-	"License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>\n"
+	"License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/"
+	"gpl.html>\n"
 	"This is free software: you are free to change and redistribute it.\n"
 	"There is NO WARRANTY, to the extent permitted by law.");
 }
