56ab175174f81bc41c60bdc47fd828ab27e81330
Andrea G. Monaco <andrea.monaco@autistici.org>
2023-02-23 16:50:30 +0100

    Add test

--- test.pl
+++ test.pl
@@ -411,6 +411,7 @@ make_test ("(cadddr '(0 1 2 3 4))", "3");
 make_test ("(caar '((4) 1 2 3))", "4");
 make_test ("(incf c)", "6");
 make_test ("(incf c 3)", "9");
+make_test ("(let ((i 0)) (incf i))", "1");
 make_test ("(decf c)", "8");
 make_test ("(decf c c)", "0");
 make_test ("(and)", "T");
