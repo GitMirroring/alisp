93b14b98e34d99de2fc937029c5c57b9e4338995
Andrea G. Monaco <andrea.monaco@autistici.org>
2023-05-25 20:27:45 +0200

    Add LAMBDA-LIST-KEYWORDS constant

--- cl.lisp
+++ cl.lisp
@@ -34,6 +34,10 @@
 (defconstant CHAR-CODE-LIMIT 4294967296)
 
 
+(defconstant lambda-list-keywords '(&optional &rest &body &key &allow-other-keys))
+
+
+
 (defun identity (x) x)
 
 
@@ -631,24 +635,24 @@
 
 (export '(lambda-parameters-limit call-arguments-limit multiple-values-limit
 	  array-rank-limit array-dimension-limit array-total-size-limit
-	  char-code-limit identity pi 1+ 1- minusp plusp abs zerop mod rem evenp
-	  oddp isqrt conjugate *gensym-counter* gensym first second third fourth
-	  fifth sixth seventh eighth ninth tenth rest caar cadr cdar cddr caaar
-	  caadr cadar caddr cdaar cdadr cddar cdddr caaaar caaadr caadar caaddr
-	  cadaar cadadr caddar cadddr cdaaar cdaadr cdadar cdaddr cddaar cddadr
-	  cdddar cddddr endp when unless incf decf and or cond return every some
-	  notany notevery member member-if member-if-not find find-if
-	  find-if-not assoc assoc-if assoc-if-not position position-if
-	  position-if-not count count-if count-if-not remove remove-if-not
-	  delete delete-if delete-if-not nreverse adjoin fill push pop
-	  array-rank array-dimension array-total-size array-in-bounds-p char
-	  string/= char-equal digit-char digit-char-p char-int string-upcase
-	  string-downcase string-capitalize nstring-upcase nstring-downcase
-	  nstring-capitalize consp listp symbolp keywordp functionp packagep
-	  integerp rationalp floatp complexp characterp vectorp arrayp sequencep
-	  stringp hash-table-p pathnamep streamp realp numberp equal equalp
-	  fdefinition complement mapc terpri write-line write-sequence prin1
-	  princ print format))
+	  char-code-limit lambda-list-keywords identity pi 1+ 1- minusp plusp
+	  abs zerop mod rem evenp oddp isqrt conjugate *gensym-counter* gensym
+	  first second third fourth fifth sixth seventh eighth ninth tenth rest
+	  caar cadr cdar cddr caaar caadr cadar caddr cdaar cdadr cddar cdddr
+	  caaaar caaadr caadar caaddr cadaar cadadr caddar cadddr cdaaar cdaadr
+	  cdadar cdaddr cddaar cddadr cdddar cddddr endp when unless incf decf
+	  and or cond return every some notany notevery member member-if
+	  member-if-not find find-if find-if-not assoc assoc-if assoc-if-not
+	  position position-if position-if-not count count-if count-if-not
+	  remove remove-if-not delete delete-if delete-if-not nreverse adjoin
+	  fill push pop array-rank array-dimension array-total-size
+	  array-in-bounds-p char string/= char-equal digit-char digit-char-p
+	  char-int string-upcase string-downcase string-capitalize
+	  nstring-upcase nstring-downcase nstring-capitalize consp listp symbolp
+	  keywordp functionp packagep integerp rationalp floatp complexp
+	  characterp vectorp arrayp sequencep stringp hash-table-p pathnamep
+	  streamp realp numberp equal equalp fdefinition complement mapc terpri
+	  write-line write-sequence prin1 princ print format))
 
 
 
