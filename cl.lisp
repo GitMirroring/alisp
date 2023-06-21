f534467516d9005b86af8fed377b6926accd665b
Andrea G. Monaco <andrea.monaco@autistici.org>
2023-06-21 17:11:24 +0200

    Add MULTIPLE-VALUE-BIND

--- cl.lisp
+++ cl.lisp
@@ -211,6 +211,13 @@
   `(return-from nil (values-list (multiple-value-list ,val))))
 
 
+(defmacro multiple-value-bind (vars valform &body forms)
+  (let ((restsym (gensym)))
+    `(multiple-value-call (lambda (&optional ,@vars &rest ,restsym)
+			    ,@forms)
+       ,valform)))
+
+
 (defun every (pred &rest sequences)
   (let ((n (apply 'min (mapcar #'length sequences))))
     (dotimes (i n)
@@ -657,18 +664,18 @@
 	  caar cadr cdar cddr caaar caadr cadar caddr cdaar cdadr cddar cdddr
 	  caaaar caaadr caadar caaddr cadaar cadadr caddar cadddr cdaaar cdaadr
 	  cdadar cdaddr cddaar cddadr cdddar cddddr endp when unless incf decf
-	  and or cond return every some notany notevery member member-if
-	  member-if-not find find-if find-if-not assoc assoc-if assoc-if-not
-	  position position-if position-if-not count count-if count-if-not
-	  remove remove-if-not delete delete-if delete-if-not nreverse adjoin
-	  fill push pop array-rank array-dimension array-total-size
-	  array-in-bounds-p char string/= char-equal digit-char digit-char-p
-	  char-int string-upcase string-downcase string-capitalize
+	  and or cond return multiple-value-bind every some notany notevery
+	  member member-if member-if-not find find-if find-if-not assoc assoc-if
+	  assoc-if-not position position-if position-if-not count count-if
+	  count-if-not remove remove-if-not delete delete-if delete-if-not
+	  nreverse adjoin fill push pop array-rank array-dimension
+	  array-total-size array-in-bounds-p char string/= char-equal digit-char
+	  digit-char-p char-int string-upcase string-downcase string-capitalize
 	  nstring-upcase nstring-downcase nstring-capitalize consp listp symbolp
 	  keywordp functionp packagep integerp rationalp floatp complexp
 	  characterp vectorp arrayp sequencep stringp hash-table-p pathnamep
-	  streamp realp numberp equal equalp fdefinition complement
-	  mapc terpri write-line write-sequence prin1 princ print format))
+	  streamp realp numberp equal equalp fdefinition complement mapc terpri
+	  write-line write-sequence prin1 princ print format))
 
 
 
