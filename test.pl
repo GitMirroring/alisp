ce1be1a69901bb5d395b63ef64d4ab43c1a5548f
Andrea G. Monaco <andrea.monaco@autistici.org>
2023-06-21 16:24:22 +0200

    Add test

--- test.pl
+++ test.pl
@@ -185,6 +185,7 @@ make_test ("(values (+ 1 0) 2 3)", "1\n2\n3");
 make_test ("(values-list '(1 2 3))", "1\n2\n3");
 make_test ("(multiple-value-list (values))", "NIL");
 make_test ("(multiple-value-list (values 1 2 3))", "(1 2 3)");
+make_test ("(multiple-value-call #'+ (floor 5 2) (values) 1)", "4");
 make_test ("(if nil 1)", "NIL");
 make_test ("(common-lisp:if '(1) 2)", "2");
 make_test ("(cl:eq 1 1)", "NIL");
