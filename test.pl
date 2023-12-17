6ecd08b99e29c2fc28e7c80ec533307090cbf3b2
Andrea G. Monaco <andrea.monaco@autistici.org>
2023-12-17 17:27:28 +0100

    Add tests

--- test.pl
+++ test.pl
@@ -796,6 +796,7 @@ make_test ("#'ship-x", "#<FUNCTION SHIP-X>");
 make_test ("(defparameter s1 (make-ship))", "S1");
 make_test ("s1", "#<STRUCTURE OF CLASS SHIP>");
 make_test ("(class-of s1)", "#<STRUCTURE CLASS SHIP>");
+make_test ("(type-of s1)", "SHIP");
 make_test ("(typep s1 'ship)", "T");
 make_test ("(typep 0 'ship)", "NIL");
 make_test ("(ship-x s1)", "NIL");
@@ -809,6 +810,7 @@ make_test ("(find-class 'starship)", "#<STANDARD-CLASS STARSHIP>");
 make_test ("(defparameter inst (make-instance 'starship))", "INST");
 make_test ("inst", "#<STARSHIP OBJECT ...>");
 make_test ("(class-of inst)", "#<STANDARD-CLASS STARSHIP>");
+make_test ("(type-of inst)", "STARSHIP");
 make_test ("(setf (slot-value inst 'x) 0)", "0");
 make_test ("(setf (slot-value inst 'y) 2.5)", "2.5");
 make_test ("(slot-value inst 'x)", "0");
@@ -821,6 +823,7 @@ make_test ("(genfun 0 nil nil)", "11");
 make_test ("(define-condition new-math-error (arithmetic-error) (a b))", "NEW-MATH-ERROR");
 make_test ("(make-condition 'new-math-error)", "#<CONDITION OF CLASS NEW-MATH-ERROR>");
 make_test ("(class-of (make-condition 'new-math-error))", "#<CONDITION-CLASS NEW-MATH-ERROR>");
+make_test ("(type-of (make-condition 'new-math-error))", "NEW-MATH-ERROR");
 make_test ("(tagbody\n" .
 	   "  (write \"1\")\n" .
 	   "  (go jmp)\n" .
@@ -1226,6 +1229,7 @@ make_test ("(oddp 0)", "NIL");
 make_test ("(oddp -1)", "T");
 make_test ("*random-state*", "#<RANDOM-STATE ?>");
 make_test ("(make-random-state)", "#<RANDOM-STATE ?>");
+make_test ("(type-of (make-random-state))", "RANDOM-STATE");
 make_test ("(make-random-state t)", "#<RANDOM-STATE ?>");
 make_test ("(make-random-state nil)", "#<RANDOM-STATE ?>");
 make_test ("(make-random-state *random-state*)", "#<RANDOM-STATE ?>");
