4c3d06f44880d46e157fda9ddf1dd172deca2d2a
Andrea G. Monaco <andrea.monaco@autistici.org>
2024-03-17 22:58:01 +0100

    Add tests

--- test.pl
+++ test.pl
@@ -1020,6 +1020,8 @@ make_test ("(defmacro testmac2 nil '(loop for i to 5 do (write i)))", "TESTMAC2"
 make_test ("(defun testfun2 nil (testmac2))", "TESTFUN2");
 make_test ("(compile 'testfun2)", "TESTFUN2\nNIL\nNIL");
 make_test ("(testfun2)", "012345\nNIL");
+make_test ("(defun testfun3 nil)", "TESTFUN3");
+make_test ("(compile 'testfun3)", "TESTFUN3\nNIL\nNIL");
 make_test ("(write \"\")", "\"\"\n\"\"");
 make_test ("(write-string \"aaa\\n\")", "aaan\n\"aaan\"");
 make_test ("(write-string \"\n" .
