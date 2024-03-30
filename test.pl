6ec152d7a91d5b8fd1d3fb8ef7e2f882a2b2a80d
Andrea G. Monaco <andrea.monaco@autistici.org>
2024-03-30 14:55:57 +0100

    Add test

--- test.pl
+++ test.pl
@@ -1608,6 +1608,7 @@ make_test ("#+common-lisp '(1\n2)", "(1 2)");
 make_test ("#-common-lisp (\n) 0", "0");
 make_test ("#+foo \"aaa\nbbb\" 0", "0");
 make_test ("#+foo (1\n2 (3)) 0", "0");
+make_test ("#+foo (\"\") 0", "0");
 make_test ("#-fooo :fooo 0", ":FOOO\n0");
 
 make_test ("*package*", "#<PACKAGE \"COMMON-LISP-USER\">");
