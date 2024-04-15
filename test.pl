ea59d65b2d2aef205d6702b825198b3995719735
Andrea G. Monaco <andrea.monaco@autistici.org>
2024-04-15 16:36:56 +0200

    Add test

--- test.pl
+++ test.pl
@@ -1555,6 +1555,7 @@ make_test ("(push (1+ 0) ll)", "(1)");
 make_test ("ll", "(1)");
 make_test ("(let ((l '(1 2 3 4))) (pushnew 3 l :test #'=))", "(1 2 3 4)");
 make_test ("(let ((l '(1 2 3 4))) (pushnew 0 l :test #'= :key 'identity))", "(0 1 2 3 4)");
+make_test ("(let ((l '((1 2 3)))) (pushnew 0 (car l)))", "(0 1 2 3)");
 make_test ("(pop ll)", "1");
 make_test ("ll", "NIL");
 make_test ("(set-difference '(0 1 1 2) nil)", "(0 1 1 2)");
