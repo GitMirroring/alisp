6c4a0b1e8f50bfb90f48b048780309cff4622495
Andrea G. Monaco <andrea.monaco@autistici.org>
2024-05-18 16:31:48 +0200

    Add tests

--- test.pl
+++ test.pl
@@ -1479,6 +1479,10 @@ make_test ("(restart-bind ((foo (lambda nil))) (restart-bind nil) (invoke-restar
 make_test ("(restart-bind ((foo (lambda nil 0)) (foo (lambda nil 1))) (invoke-restart 'foo))", "0");
 make_test ("(restart-bind ((rest1 (lambda nil 10)) (rest2 (lambda nil 20)) (rest2 (lambda nil 30))) (invoke-restart 'rest2))", "20");
 make_test ("(restart-bind ((foo (lambda nil (write-line \"hi\") (abort)))) (invoke-restart 'foo))", "hi");
+make_test ("(restart-case (values 1 2) (rest1 (x y) 0) (rest2 (x) 10))", "1\n2");
+make_test ("(restart-case (invoke-restart 'rest2 0 1) (rest1 (x) 10) (rest2 (x y) (values x y)) (rest2 (x) 10))", "0\n1");
+make_test ("(restart-case (invoke-restart 'rest1 0 1) (rest1 (x y)) (rest1 (x y) 10))", "NIL");
+make_test ("(restart-bind ((rest1 (lambda (x) 20))) (restart-case (invoke-restart 'rest2 0 1) (rest2 (x y) (invoke-restart 'rest1 40)) (rest1 (x y) 30)))", "20");
 make_test ("(block test (unwind-protect 0 (write 'whatever)))", "WHATEVER\n0");
 make_test ("(block test (unwind-protect (return-from test 0) (write 'whatever)))", "WHATEVER\n0");
 make_test ("(block test (unwind-protect (return-from test (values 0 1)) (write 'whatever)))", "WHATEVER\n0\n1");
