cb77d50cf3da754b1f801278d322e756e1dab0d5
Andrea G. Monaco <andrea.monaco@autistici.org>
2025-01-29 12:03:11 +0100

    Add tests

--- test.pl
+++ test.pl
@@ -1238,6 +1238,8 @@ make_test ("(defun testfun6 nil (if 0 (cond (nil))))", "TESTFUN6");
 make_test ("(compile 'testfun6)", "TESTFUN6\nNIL\nNIL");
 make_test ("(testfun6)", "NIL");
 make_test ("(with-compilation-unit nil (write 0) (write 1))", "01\n1");
+make_test ("(compile-file-pathname \"foo.lisp\")", "#P\"foo.alc\"");
+make_test ("(compile-file-pathname \"foo/bar.lisp\")", "#P\"foo/bar.alc\"");
 make_test ("(write \"\")", "\"\"\n\"\"");
 make_test ("(write 'hello :stream *error-output*)", "HELLO\nHELLO");
 make_test ("(defun f nil (write 0) (write 1))", "F");
