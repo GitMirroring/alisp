60a55031e9ca1d43b6a28be7a122a5abf384fe23
Andrea Monaco <monacoandrea94@gmail.com>
2025-05-25 09:18:59 +0200

    Add test

--- test.pl
+++ test.pl
@@ -756,6 +756,7 @@ make_test ("(symbol-function 'if)", "#<SPECIAL OPERATOR IF>");
 make_test ("(flet ((foo nil)) (symbol-function 'car))", "#<FUNCTION BUILTIN CAR>");
 make_test ("(fdefinition 'fun)", "#<FUNCTION FUN>");
 make_test ("(fdefinition 'car)", "#<FUNCTION BUILTIN CAR>");
+make_test ("(fdefinition '(setf car))", "#<FUNCTION BUILTIN (SETF CAR)>");
 make_test ("(funcall (complement #'numberp) \"\")", "T");
 make_test ("(symbol-name 'aaa)", "\"AAA\"");
 make_test ("(symbol-name :bbb)", "\"BBB\"");
