905ccda9efd48d63922d7b6783beef82e9ed42e1
Andrea Monaco <monacoandrea94@gmail.com>
2026-05-23 15:21:00 +0200

    Add test

--- test.pl
+++ test.pl
@@ -1054,6 +1054,7 @@ make_test ("(make-array '(2 3))", "#2A((NIL NIL NIL) (NIL NIL NIL))");
 make_test ("(let ((*print-array* nil)) (write (make-array '(1 2 3))))", "#<ARRAY, RANK 3>\n#3A(((NIL NIL NIL) (NIL NIL NIL)))");
 make_test ("(make-array '(1 2 3))", "#3A(((NIL NIL NIL) (NIL NIL NIL)))");
 make_test ("(make-array nil)", "#0ANIL");
+make_test ("(make-array 3 :adjustable t)", "#(NIL NIL NIL)");
 make_test ("(make-array '(2 3 2) :initial-contents '(((a b) (c d) (e f)) ((g h) (i j) (k l))))", "#3A(((A B) (C D) (E F)) ((G H) (I J) (K L)))");
 make_test ("(make-array '(2 3 2) :initial-contents #(((a b) #(c d) \"ef\") ((g h) #(i j) #*10)))", "#3A(((A B) (C D) (#\\e #\\f)) ((G H) (I J) (1 0)))");
 make_test ("(make-array 3 :initial-contents '(a b c))", "#(A B C)");
