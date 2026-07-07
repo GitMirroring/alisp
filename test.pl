1f99f7cbbad7316d9f63ca466c4007ab7f1f8948
Andrea Monaco <monacoandrea94@gmail.com>
2026-07-07 20:39:02 +0200

    Update test

--- test.pl
+++ test.pl
@@ -1892,7 +1892,7 @@ make_test ("(defmethod genfun ((x string) y z) 10)", "#<STANDARD-METHOD GENFUN (
 make_test ("(defmethod genfun ((x integer) y z) 11)", "#<STANDARD-METHOD GENFUN (INTEGER T T)>");
 make_test ("(genfun \"\" nil nil)", "10");
 make_test ("(genfun 0 nil nil)", "11");
-make_test ("(defgeneric genfun (x))", "#<STANDARD-GENERIC-FUNCTION GENFUN>");
+make_test ("(defgeneric genfun (x))", "warning: redefining generic function GENFUN\n#<STANDARD-GENERIC-FUNCTION GENFUN>");
 make_test ("(defmethod genfun2 ((x string) y z) 12)", "#<STANDARD-METHOD GENFUN2 (STRING T T)>");
 make_test ("(defmethod genfun2 ((x integer) y z) 13)", "#<STANDARD-METHOD GENFUN2 (INTEGER T T)>");
 make_test ("(defgeneric genfun3 (x y z))", "#<STANDARD-GENERIC-FUNCTION GENFUN3>");
