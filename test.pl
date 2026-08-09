afcf3d01337e0b43bc30fe64ee5b20d3fe660ab6
Andrea Monaco <monacoandrea94@gmail.com>
2026-08-09 16:15:28 +0200

    Add test

--- test.pl
+++ test.pl
@@ -2428,6 +2428,7 @@ make_test ("`(+ #-foo ,0)", "(+ 0)");
 make_test ("(copy-readtable nil)", "#<READTABLE ...>");
 make_test ("(copy-readtable (copy-readtable nil) (copy-readtable nil))", "#<READTABLE ...>");
 make_test ("(copy-readtable)", "#<READTABLE ...>");
+make_test ("(let ((readt (copy-readtable nil))) (setf (readtable-case readt) :invert) (readtable-case readt))", ":INVERT");
 
 make_test ("(encode-universal-time 10 10 10 1 2 1900 0)", "2715010");
 make_test ("(encode-universal-time 10 11 12 19 8 2004 0)", "3301906270");
