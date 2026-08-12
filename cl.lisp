28e0a52ee897c8b618b03000c6da10bd4d3f5da1
Andrea Monaco <monacoandrea94@gmail.com>
2026-08-12 19:02:05 +0200

    Make DESCRIBE-OBJECT a generic function as required by ANSI

--- cl.lisp
+++ cl.lisp
@@ -191,12 +191,6 @@
        (error (format nil "don't know how to require ~a" mod))))))
 
 
-(defun describe (obj &optional str) (values))
-(defun describe-object (obj str))
-(defun inspect (obj))
-
-
-
 ;;; the following useless constants are just ridiculously high because lambda
 ;;; lists and multiple values use chained lists, so there's no hard limit
 
@@ -4066,6 +4060,11 @@
 
 
 
+(defun describe (obj &optional str) (values))
+(defgeneric describe-object (obj str))
+(defun inspect (obj))
+
+
 
 (dolist (sym '(while split-pathname al::list-directory-with-full-path
 	       exclude-directory al::list-subdirs indent print-object-nicely
@@ -4079,18 +4078,18 @@
     (sym '(*features* *default-pathname-defaults* defmacro in-package defun
            defgeneric add-method defmethod machine-instance machine-type
            machine-version short-site-name long-site-name *modules* provide
-           require describe describe-object inspect lambda-parameters-limit
-           call-arguments-limit multiple-values-limit array-rank-limit
-           array-dimension-limit array-total-size-limit char-code-limit
-           lambda-list-keywords first second third fourth fifth sixth seventh
-           eighth ninth tenth rest caar cadr cdar cddr caaar caadr cadar caddr
-           cdaar cdadr cddar cdddr caaaar caaadr caadar caaddr cadaar cadadr
-           caddar cadddr cdaaar cdaadr cdadar cdaddr cddaar cddadr cdddar cddddr
-           cond identity constantly *read-default-float-format* pi 1+ 1- minusp
-           plusp abs zerop signum mod rem evenp oddp rationalize gcd lcm isqrt
-           conjugate cis upgraded-complex-part-type logand logandc1 logandc2
-           logeqv lognand lognor logorc1 logorc2 logxor logbitp ldb-test boole-1
-           boole-2 boole-andc1 boole-andc2 boole-and boole-c1 boole-c2 boole-clr
+           require lambda-parameters-limit call-arguments-limit
+           multiple-values-limit array-rank-limit array-dimension-limit
+           array-total-size-limit char-code-limit lambda-list-keywords first
+           second third fourth fifth sixth seventh eighth ninth tenth rest caar
+           cadr cdar cddr caaar caadr cadar caddr cdaar cdadr cddar cdddr caaaar
+           caaadr caadar caaddr cadaar cadadr caddar cadddr cdaaar cdaadr cdadar
+           cdaddr cddaar cddadr cdddar cddddr cond identity constantly
+           *read-default-float-format* pi 1+ 1- minusp plusp abs zerop signum
+           mod rem evenp oddp rationalize gcd lcm isqrt conjugate cis
+           upgraded-complex-part-type logand logandc1 logandc2 logeqv lognand
+           lognor logorc1 logorc2 logxor logbitp ldb-test boole-1 boole-2
+           boole-andc1 boole-andc2 boole-and boole-c1 boole-c2 boole-clr
            boole-eqv boole-ior boole-nand boole-nor boole-orc1 boole-orc2
            boole-set boole-xor boole integer-length ash *gensym-counter* gensym
            gentemp make-list copy-alist copy-tree tree-equal sublis nsublis endp
@@ -4144,7 +4143,8 @@
            stream-error-stream arithmetic-error-operation
            arithmetic-error-operands simple-condition-format-control
            simple-condition-format-arguments break ignore-errors abort continue
-           muffle-warning variable structure compiler-macro documentation))
+           muffle-warning variable structure compiler-macro documentation
+           describe describe-object inspect))
   (export sym)
   (if (fboundp sym)
       (compile sym)))
